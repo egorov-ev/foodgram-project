2ec6205749ae5f02a8cc1fa8c9551ae2c5e2066d
egorovev <!4>
2021-01-31 11:52:25 +0300

    Commit 9: after project rebuild, in progress bugfix

---
+++ recipes/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Recipe
+
+
+class RecipeForm(ModelForm):
+    class Meta(object):
+        model = Recipe
+        fields = ['title', 'tags', 'ingredients', 'cooking_time', 'text',
+                  'image', ]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ['text', ]
