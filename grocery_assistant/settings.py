5b9c6a5a0aa9e2963781d65f77f051e2762da2a8
egorovev <!4>
2021-02-20 20:11:08 +0300

    Commit 20: add page: author.html, tech.html, fix resetPassword.html and other flow

--- grocery_assistant/settings.py
+++ grocery_assistant/settings.py
@@ -122,13 +122,13 @@ LOGIN_REDIRECT_URL = 'index'
 
 PAGINATION_PAGE_SIZE = 3
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#
-# EMAIL_HOST = os.environ['EMAIL_HOST']
-# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-# EMAIL_PORT = os.environ['EMAIL_PORT']
-# EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
 
 # Отладка проекта
 
